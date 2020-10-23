19d7771ad1b20756356986e60d020b0ad63fe424
vistritpandey <vistritpandey@gmail.com>
2020-10-23 13:21:03 +0530

    config file added

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB27AtBtakvhiTqbyPeNznnjtE2K3Ar-8E",
+    authDomain: "vistrit-facebook-clone.firebaseapp.com",
+    databaseURL: "https://vistrit-facebook-clone.firebaseio.com",
+    projectId: "vistrit-facebook-clone",
+    storageBucket: "vistrit-facebook-clone.appspot.com",
+    messagingSenderId: "957743779944",
+    appId: "1:957743779944:web:089637a01c02caea5dabdd",
+    measurementId: "G-VT9S817GKC"
+  };
