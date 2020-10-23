03c89df9793e908f61e11ff656fd98b59744b3b7
vistritpandey <vistritpandey@gmail.com>
2020-10-23 13:31:00 +0530

    m

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './Header'
 
 function App() {
   return (
     <div className="App">
       <p>Hello darkness my old friend</p>
-      {/* Header */}
+      <Header />
       {/* App Body */}
       {/* Sidebar */}
       {/* Feed */}
