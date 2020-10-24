d7f13da49b447938d34ebbf9298a9197573d2bc7
vistritpandey <vistritpandey@gmail.com>
2020-10-24 14:52:19 +0530

    m

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import Sidebar from './Sidebar'
 import Feed from './Feed'
 import Widget from './Widget'
 import Login from './Login'
+import { useStateValue } from './StateProvider';
 
 function App() {
 
-  const user = null;
+  const [{user}, dispatch] = useStateValue();
   return (
     <div className="App">
     {!user ? (
--- src/Login.js
+++ src/Login.js
@@ -2,11 +2,17 @@ import React from 'react'
 import './Login.css'
 import Button from "@material-ui/core/Button"
 import {auth , provider } from './firebase'
+import {actionTypes} from './reducer'
+import {useStateValue} from './StateProvider'
 
 function Login() {
+    const [state, dispatch] = useStateValue();
     const signIn = () =>{
         auth.signInWithPopup(provider).then(result => {
-            console.log(result)
+            dispatch({
+                type: actionTypes.SET_USER,
+                user: result.user,
+            });
         }).catch(error => alert(error.message));
     };
     return (
