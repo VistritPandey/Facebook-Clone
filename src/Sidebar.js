f3716d1b2cdd388d91a5f05a7b1a7c51b130f2c5
vistritpandey <vistritpandey@gmail.com>
2020-10-24 08:28:16 +0530

    m

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import './Sidebar.css'
 import SidebarRow from './SidebarRow'
+import LocalHospitalIcon from "@material-ui/icons/LocalHospital"
+import EmojiFlagsIcon from "@material-ui/icons/EmojiFlags"
+import PeopleIcon from "@material-ui/icons/People"
+import ChatIcon from "@material-ui/icons/Chat"
+import StorefrontIcon from "@material-ui/icons/Storefront"
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary"
+import ExpandMoreOutlined from "@material-ui/icons/ExpandMoreOutlined"
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <SidebarRow title='Pages'/>
-            <SidebarRow title='Friends'/>
-            <SidebarRow title=''/>
-            <SidebarRow title=''/>
-            <SidebarRow title=''/>
-            <SidebarRow title=''/>
+            <SidebarRow src = '' title ='Vistrit' />
+            <SidebarRow Icon={LocalHospitalIcon} title="COVID-19 Info Center"/>
+            <SidebarRow Icon={EmojiFlagsIcon} title='Pages'/>
+            <SidebarRow Icon={PeopleIcon} title='Friends'/>
+            <SidebarRow Icon={ChatIcon} title='Messenger'/>
+            <SidebarRow Icon={StorefrontIcon} title='Marketplace'/>
+            <SidebarRow Icon={VideoLibraryIcon} title='Videos'/>
+            <SidebarRow Icon={ExpandMoreOutlined} title='More'/>
         </div>
     )
 }
--- src/SidebarRow.js
+++ src/SidebarRow.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './SidebarRow.css'
+import {Avatar} from "@material-ui/core"
 
 function SidebarRow({src, Icon, title}) {
     return (
         <div className="sidebarRow">
-            <p>{title}</p>
+        {src && <Avatar src ={src} />}
+        {Icon && <Icon />}
+            <h4>{title}</h4>
         </div>
     )
 }
