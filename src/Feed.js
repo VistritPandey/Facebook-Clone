173b5ec05c374e66c7a34a16ffe722d4f27f1531
vistritpandey <vistritpandey@gmail.com>
2020-10-24 10:19:11 +0530

    OKAY

--- src/Feed.js
+++ src/Feed.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Feed.css'
+import MessageSender from './MessageSender'
 import StoryReel from './StoryReel'
 
 function Feed() {
     return (
         <div className="feed">
             <StoryReel />
-            {/*MessageSender*/}
+            <MessageSender />
         </div>
     )
 }
---
+++ src/MessageSender.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './MessageSender.css'
+
+function MessageSender() {
+    return (
+        <div className="MessageSender">
+            
+        </div>
+    )
+}
+
+export default MessageSender
