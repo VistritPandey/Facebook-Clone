99aa250ba95aea2778c04e702010d3293a8b1ac4
vistritpandey <vistritpandey@gmail.com>
2020-10-24 09:22:24 +0530

    m

--- src/Story.js
+++ src/Story.js
@@ -4,8 +4,8 @@ import {Avatar} from "@material-ui/core"
 
 function story({image, profileSrc, title}) {
     return (
-        <div className="story">
-            <Avatar src={profileSrc} />
+        <div style={{backgroundImage:`url(${image})`}} className="story">
+            <Avatar className="story__avatar" src={profileSrc} />
             <h4>{title}</h4>
         </div>
     )
